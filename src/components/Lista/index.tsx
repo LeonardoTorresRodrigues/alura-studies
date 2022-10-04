b7277c5c95d1b539d5da517d03355500a08d3707
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-10-04 11:21:58 -0300

    Estilizando itens da lista

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -17,7 +17,7 @@ function Lista() {
       <h2> Estudos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index}>
+          <li key={index} className="item">
             <h3> {item.tarefa} </h3>
             <span> {item.tempo} </span>
           </li>
