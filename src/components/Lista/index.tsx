9f6db06a70e535ac231bc5e1f6a8a770a47a42fc
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-10-06 11:40:50 -0300

    Declarando um estado para a lista

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import Item from "./Item";
 import style from './Lista.module.scss';
 
 export default function Lista() {
-  const tarefas = [{
+  const [tarefas, setTarefas] = useState([{
     tarefa: 'React',
     tempo: '02:00:00'
   }, {
@@ -11,13 +12,15 @@ export default function Lista() {
   }, {
     tarefa: 'TypeScript',
     tempo: '03:00:00'
-  }]
+  }]);
   return (
     <aside className={style.listaTarefas}>
-      <h2> Estudos do dia </h2>
+      <h2 onClick={() => {
+        setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00" }])
+      }}> Estudos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
-          <Item 
+          <Item
             key={index}
             {...item}
           />
